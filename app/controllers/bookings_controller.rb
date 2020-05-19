038681055c1cf19de0da84e4dc06cf0acbaf01cc
hugues delab <hugues_delab@hotmail.fr>
2020-05-19 14:51:30 +0200

    impl routes/create meth

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,9 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new
+    @travel = Travel.find(params[:travel_id])
+    @booking.travel = @travel
+    @booking.save
+    redirect_to travel_path(@travel)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :travels
+  resources :travels do
+    resources :bookings, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #Destroy a travel
