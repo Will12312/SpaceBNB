bbec098e1b3b8840eaefa6fd980db982a8888072
William GUILLERM <guillerm.william@gmail.com>
2020-05-19 14:43:40 +0200

    destroy booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,13 @@
 class BookingsController < ApplicationController
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to travel_path(@booking.travel)
+  end
+
+    private
+
+  def dose_params
+    params.require(:booking).permit(:travel_id, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :travels
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  #Destroy a travel
+  resources :travels do
+    resources :bookings, only:[:new, :create, :destroy]
+  end
+  resources :bookings, only:[:destroy]
 
 end
