4f40d825951d7bbf7065a6a2c0e7c27f6b828dff
William GUILLERM <guillerm.william@gmail.com>
2020-05-19 14:21:48 +0200

    new create

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,36 +1,51 @@
 class TravelsController < ApplicationController
 
+  before_action :set_travel, only: [:show, :edit, :update, :destroy]
+
   def show
-    @travel = Travel.find(params[:id])
+  end
+
+  def new
+    @travel = Travel.new
+  end
+
+  def create
+    @travel = Travel.new(travel_params)
+    @travel.save
+    if @travel.save
+      redirect_to travel_path(@travel)
+    else
+      render :new
+    end
   end
 
   def index
     @travels = Travel.all
   end
-  
+
   def edit
-    @travel = Travel.find(params[:id])
   end
 
   def update
-    @travel = Travel.find(params[:id])
     @travel.update(travel_params)
-    
+
     redirect_to travel_path(@travel)
   end
 
   def destroy
-      @travel = Travel.find(params[:id])
       @travel.destroy
 
       redirect_to travels_path
   end
-  
+
    private
 
   def travel_params
-    params.require(:travel).permit(:destination, :number_of_travelers, :departure_date, :name_of_vehicle )
+    params.require(:travel).permit(:destination, :number_of_travelers, :departure_date, :name_of_vehicle, :organiser_id )
   end
-  
+
+  def set_travel
+    @travel = Travel.find(params[:id])
+  end
 
 end
