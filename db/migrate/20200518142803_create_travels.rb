7df542830f835455d30c282a728a8b29d7836d4f
William GUILLERM <guillerm.william@gmail.com>
2020-05-21 18:25:02 +0200

    ...

--- db/migrate/20200518142803_create_travels.rb
+++ db/migrate/20200518142803_create_travels.rb
@@ -5,7 +5,7 @@ class CreateTravels < ActiveRecord::Migration[6.0]
       t.string :destination
       t.integer :number_of_travelers
       t.integer :people_onboard, default: 0
-      t.date :departure_date
+      t.string :departure_date
       t.string :name_of_vehicle
 
       t.timestamps
