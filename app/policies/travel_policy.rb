1083ef4325de5a682bc30fb3ed5ec4eb5738d2bc
William Guillerm <59450083+Will12312@users.noreply.github.com>
2020-05-21 12:19:22 +0200

    Revert "added map"

--- app/policies/travel_policy.rb
+++ app/policies/travel_policy.rb
@@ -1,7 +1,7 @@
 class TravelPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.geocoded
+      scope.all
     end
   end
 
--- db/migrate/20200520160903_add_address_to_travels.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToTravels < ActiveRecord::Migration[6.0]
-  def change
-    add_column :travels, :address, :string
-  end
-end
